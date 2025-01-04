6b7acf2281652e5dfd4c0646f8f7ebbf1199f275
vitorfleonardo <vitorfleonardo@gmail.com>
2025-01-04 15:08:58 -0300

    feat: integração com backend
    Co-authored-by: melohugo <melohugo@users.noreply.github.com>
    Co-authored-by: Carlos Eduardo Mota Alves <CADU110@users.noreply.github.com>

--- Projeto/GOFs Criacionais/Prototype/frontend/src/services/api.js
+++ Projeto/GOFs Criacionais/Prototype/frontend/src/services/api.js
@@ -3,9 +3,19 @@ import axios from 'axios';
 export async function createRecurringEvents(formData) {
   try {
     const response = await axios.post(
-      'http://localhost:3000/task/create',
+      'http://localhost:8080/task/create',
       formData
     );
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao criar eventos recorrentes:', error);
+    return [];
+  }
+}
+
+export async function getEvents() {
+  try {
+    const response = await axios.get('http://localhost:8080/task/');
     return response.data;
   } catch (error) {
     console.error('Erro ao criar eventos recorrentes:', error);
